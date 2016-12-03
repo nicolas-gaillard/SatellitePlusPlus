003faaa131f560b8fd5c426497eb951038aa491f
Nicolas Gaillard <nicolas.gaillard@etu.univ-nantes.fr>
2016-12-03 17:11:03 +0100

    Squelette de la classe TimeMeasure pour la mesure de performance

---
+++ Satellite/Satellite/TimeMeasure.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <map>
+#include <utility>
+
+class TimeMeasure{
+
+private:
+
+	std::string inputFolder;	// Folder which contains all executables				
+	std::ifstream * nameOutput;		// File which will display results
+	std::map<int, std::pair<long, long> > resultTabs;
+	std::ifstream * inputData;	// File which contains data of 1 simulation
+
+public:
+	// Constructors and destructor :
+	// -----------------------------
+	TimeMeasure(std::string, std::string, std::string);
+	~TimeMeasure();
+
+	// Functions :
+	// -----------
+	
+	// Measures the execution time of 1 executable on 1 data set :
+	long measureExec(std::string pathExecutable, std::string outputExec);
+
+	// Launch each executable :
+	bool ExecuteFolder();
+
+	// Create the file where all the results are displayed :
+	bool createResults();
+};
